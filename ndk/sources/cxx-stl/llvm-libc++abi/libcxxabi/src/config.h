eb7834cbd8cd0ac440d17776c411a7e6a550967e
John Budorick <jbudorick@google.com>
2016-06-07 14:59:47 -0700

    Revert "Roll NDK to r11c and extract it into its own repository."
    I reverted the roll in chromium and may not get a chance to attempt to
    reland it before someone needs to make a change to this repository.
    
    This reverts commit 04c2c06fe3ef3d93343feb6e83cd932725740d7f.
    
    Change-Id: Ic040bd4b7bb397f7c822cdb78866c7ea270bc3f4

---
+++ ndk/sources/cxx-stl/llvm-libc++abi/libcxxabi/src/config.h
@@ -0,0 +1,47 @@
+//===----------------------------- config.h -------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//
+//  Defines macros used within the libc++abi project.
+//
+//===----------------------------------------------------------------------===//
+
+
+#ifndef LIBCXXABI_CONFIG_H
+#define LIBCXXABI_CONFIG_H
+
+#include <unistd.h>
+
+// Set this in the CXXFLAGS when you need it
+#if !defined(LIBCXXABI_HAS_NO_THREADS)
+#  define LIBCXXABI_HAS_NO_THREADS 0
+#endif
+
+// Set this in the CXXFLAGS when you need it, because otherwise we'd have to
+// #if !defined(__linux__) && !defined(__APPLE__) && ...
+// and so-on for *every* platform.
+#ifndef LIBCXXABI_BAREMETAL
+#  define LIBCXXABI_BAREMETAL 0
+#endif
+
+// -- BEGIN Taken from gabixx_config.h for gcc compat
+// Clang provides __sync_swap(), but GCC does not.
+// IMPORTANT: For GCC, __sync_lock_test_and_set has acquire semantics only
+// so an explicit __sync_synchronize is needed to ensure a full barrier.
+// TODO(digit): Use __atomic_swap_acq_rel when available.
+#if !defined(__clang__)
+#  define __sync_swap(address, value)  \
+  __extension__ ({ \
+    __typeof__(*(address)) __ret = __sync_lock_test_and_set((address),(value)); \
+    __sync_synchronize(); \
+    __ret; \
+  })
+#endif
+// -- END Taken from gabixx_config.h for gcc compat
+
+
+#endif // LIBCXXABI_CONFIG_H
